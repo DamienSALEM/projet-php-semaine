d8e228844ae052b356b768c18a0343ab5e41d0d6
ThomasDev <thomas.jallu@gmail.com>
2021-11-30 17:34:45 +0100

    Class au mauvais endroit

--- Model/UserClass.php
+++ Model/UserClass.php
@@ -1,36 +1,5 @@
 <?php
 
-class User {
 
-    public function __construct() {
-
-        $this->pdo = (new Database)->getPDO();
-    
-    }
-
-    public function getUsers() {
-
-        $query = $this->pdo->query("SELECT * FROM user");
-        $query->setFetchMode(\PDO::FETCH_OBJ); // retourne les valeurs en objet
-        $users = $query->fetchAll();
-
-    }
-
-    public function getUser(int $id) {
-
-        $query = $this->pdo->query("SELECT * FROM user WHERE id=$id");
-        $query->setFetchMode(\PDO::FETCH_OBJ);
-        $user = $query->fetch();
-    }
-
-    public function addUser(string $lastName, string $firstName, string $email, string $password) {
-
-        if(!empty($lastName) && !empty($firstName) && !empty($email) && !empty($password)) {
-            $prepare = $this->pdo->prepare("INSERT INTO user VALUES (null, $firstName, $lastNames, $email, $password)");
-            $prepare->execute($user);
-        }
-
-    }
-}
 
 ?>
--- Model/WineClass.php
+++ Model/WineClass.php
@@ -1,34 +1,3 @@
 <?php
 
-class Wine {
-
-    public function __construct() {
-
-        $this->pdo = (new Database)->getPDO();
-    }
-
-    public function getWines() {
-
-        $query = $this->pdo->query("SELECT * FROM product");
-        $query->setFetchMode(\PDO::FETCH_OBJ);
-        $wines = $query->fetchAll();
-    }
-
-    public function getWine() {
-
-        $query = $this->pdo->query("SELECT * FROM product");
-        $query->setFetchMode(\PDO::FETCH_OBJ);
-        $user = $query->fetchAll();
-    }
-
-    public function addWine(string $name, int $category_id, string $brand, string $origin, int $price, boolean $promotion, string $image, int $inMenu) {
-
-        if(!empty($name) && !empty($category_id) && !empty($brand) && !empty($origin) && !empty($price) && !empty($promotion) && !empty($image) && !empty($inMenu)) {
-            $prepare = $this->pdo->prepare("INSERT INTO product VALUES (null, $name, $category_id, $brand, $origin, $promotion, $image, $inMenu)");
-            $prepare->execute($user);
-        }
-    }
-
-}
-
 ?>
