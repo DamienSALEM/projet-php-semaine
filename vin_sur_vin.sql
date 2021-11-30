1b6424fe0513114475a92c4b3dbe06027cb54d1d
Damien SALEM <d.salem@ecole-ipssi.net>
2021-11-30 16:09:08 +0100

    Product and categories updated on BDD

--- vin_sur_vin.sql
+++ vin_sur_vin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 30 nov. 2021 à 11:33
+-- Généré le :  mar. 30 nov. 2021 à 15:05
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.3.5
 
@@ -73,7 +73,19 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Rouge'),
+(2, 'Rosé'),
+(3, 'Blanc'),
+(4, 'Jaune'),
+(5, 'Beaujolais'),
+(6, 'Champagne');
 
 -- --------------------------------------------------------
 
@@ -135,7 +147,25 @@ CREATE TABLE IF NOT EXISTS `products` (
   `image` varchar(256) NOT NULL,
   `is_in_menu` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `category_id`, `brand`, `origin_country`, `stock`, `price`, `promotion`, `image`, `is_in_menu`) VALUES
+(1, 'Alsace grand cru Brand vendanges tardives Riesling', 3, 'Rielsling', 'France', 100, 95, NULL, 'https://i2.cdscdn.com/pdt2/1/2/C/1/700x700/BAUMGCPINOG12C.jpg', 1),
+(2, '	Domaine de Rochemure 2016 Beaujolais Villages - Vin rouge de Bourgogne', 5, 'Domaine de Rochemure', 'France', 100, 9, NULL, 'https://i2.cdscdn.com/pdt2/N/1/0/1/700x700/CARMEN10.jpg', 1),
+(3, 'Château Cheval Blanc - Saint-Emilion 1er Grand Cru Classé - Bordeaux - 2014', 1, 'Château Cheval Blanc', 'France', 100, 3230, NULL, 'https://i2.cdscdn.com/pdt2/1/4/C/1/700x700/CHEVALBLC14C.jpg', 1),
+(4, 'Bourgogne La Chapelle Notre-Dame rosé', 2, 'La Chapelle Notre-Dame', 'France', 100, 8, NULL, 'https://i2.cdscdn.com/pdt2/9/3/5/1/700x700/AUC3299641113935.jpg', 1),
+(5, 'NICOLAS POTEL 2013 Pouilly Fuissé Vin de Bourgogne - Blanc - 75 cl', 3, 'NICOLAS POTEL', 'France', 100, 18, NULL, 'https://i2.cdscdn.com/pdt2/I/1/3/1/700x700/POTPOUFUI13.jpg', 1),
+(6, 'Domaine Christian Gros Ladoix Bourgogne 2013 - Vin rouge', 1, 'Domaine Christian Gros Ladoix', 'France', 100, 101, NULL, 'https://i2.cdscdn.com/pdt2/X/1/3/1/700x700/GROSLADOIX13.jpg', 1),
+(7, 'Gisselbrecht - AOC Alsace Gewürztraminer Grand Cru Frankstein VIN BLANC', 3, 'Gisselbrecht ', 'France', 100, 11, NULL, 'https://i2.cdscdn.com/pdt2/1/1/C/1/700x700/GISSELGEWFRAN11C.jpg', 1),
+(8, 'Château Beau-Séjour Bécot Saint-Emilion 2007', 1, 'Château Beau-Séjour Bécot', 'France', 100, 49, NULL, 'https://i2.cdscdn.com/pdt2/T/0/7/1/700x700/BBECOT07.jpg', 1),
+(9, 'Aurochs 2015 Périgord - Vin rouge du Sud-Ouest', 1, 'Aurochs 2015 Périgord', 'France', 100, 4, NULL, 'https://i2.cdscdn.com/pdt2/R/G/E/1/700x700/AURCHIGPRGE.jpg', 1),
+(10, 'MOËT & CHANDON ICE IMPÉRIAL', 6, 'MOËT & CHANDON', 'France', 100, 48, NULL, 'https://www.clos19.com/on/demandware.static/-/Sites-mh-master/default/dw4afcb324/images/large/moet_chandon/packshots/MOET-CHANDON-CHAMPAGNE-ICE-IMPERIAL-75CL-100081_0.jpg', 1),
+(11, 'VEUVE CLICQUOT LA GRANDE DAME 2012 BY YAYOI KUSAMA', 6, 'VEUVE CLICQUOT', 'France', 100, 190, NULL, 'https://www.clos19.com/on/demandware.static/-/Sites-mh-master/default/dw87485752/images/large/veuve_clicquot/packshots/VEUVE-CLICQUOT-LA-GRANDE-DAME-2012-LIMITED-EDITION-YAYOI-KUSAMA-75CL-101375_1.jpg?im=Resize,width=408,height=544', 1),
+(12, 'Côtes du Jura Vin Jaune Blanc 2011 - La Côte des Loups - Vin AOC Blanc du Jura - 62cl - Cépage Savagnin', 4, 'Cépage Savagnin', 'France', 100, 40, NULL, 'https://m.media-amazon.com/images/I/515hX0+ffdL._AC_SL1500_.jpg', 1);
 
 -- --------------------------------------------------------
 
