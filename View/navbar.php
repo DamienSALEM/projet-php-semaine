2c613761e16595865bef007ce7bdcbd4747e6b4a
Stabien <bastien.piedallu@gmail.com>
2021-12-01 09:08:48 +0100

    add navbar view

---
+++ View/navbar.php
@@ -0,0 +1,46 @@
+<header class="container-fluid navbar navbar-expand-lg">
+  <div id="toggler-container" class="d-lg-none">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbar-content"
+      aria-controls="navbar-content"
+    >
+       <span class="navbar-light navbar-toggler-icon">
+         <i class="fas fa-bars" style="color:#000; font-size:28px;"></i>
+       </span>
+    </button>
+    <img class="logo" src="../assets/logo.png" />
+  </div>
+  <nav class="collapse navbar-collapse row" id="navbar-content">
+    <ul class="navbar-nav col-12 col-xl-10 offset-xl-1">
+      <li class="nav-item">
+        <a class="nav-link">
+          <img class="side-icon" src="../assets/profile.png" />
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">Menu</a>
+      </li>
+      <li id="logo-container" class="nav-item d-lg-block">
+        <img class="logo" src="../assets/logo.png" />
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">Magasin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">Nous contacter</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">
+          <img class="side-icon" src="../assets/cart.png" />
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>
