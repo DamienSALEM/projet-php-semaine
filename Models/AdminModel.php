9eb451200356f9d85360324f464e9140954660a1
ThomasDev <thomas.jallu@gmail.com>
2021-12-01 15:41:27 +0100

    Ajout des class dans les Models

---
+++ Models/AdminModel.php
@@ -0,0 +1,49 @@
+<?php
+
+class AdminModel {
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $userName;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+
+    public function getId() {
+
+        return $this->id;
+    }
+
+    public function getUserName() {
+
+        return $this->username;
+    }
+
+    public function setUserName(string $userName) {
+
+        $this->userName = $userName;
+    }
+
+    public function getPassword() {
+        
+        return $this->password;
+    
+    }
+
+    public function setPassword(string $password) {
+
+        $this->password = $password;
+
+    }
+
+
+}
---
+++ Models/CartModel.php
@@ -0,0 +1,27 @@
+<?php
+
+class CartModel {
+
+    /**
+     * @var int
+     */
+    private $useIid;
+
+    /**
+     * @var int
+     */
+    private $produtId;
+
+    
+    public function getUserId() {
+
+        return $this->userId;
+    
+    }
+
+    public function getProductId() {
+
+        return $this->productId;
+
+    }
+}
