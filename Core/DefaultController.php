743e95dd248aa7c6c25798817e85f43391ea6c5a
Stabien <bastien.piedallu@gmail.com>
2021-12-02 11:07:55 +0100

    Add DefaultController

---
+++ Core/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+namespace Core\Controller;
+
+class DefaultController
+{
+    public function render(string $pathView, array $params = []) {
+        ob_start();
+        extract($params);
+        require ROOT."/templates/$pathView.php";
+        $content = ob_get_clean();
+        require ROOT."/templates/base.php";
+    }
+
+    public function redirectToRoute($path) {
+        header("Location: $path");
+    }
+}
