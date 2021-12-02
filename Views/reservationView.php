1538a852d96f08ea58efa65f4186fc0b86e1a1d8
Yann Doan <honza.doan@gmail.com>
2021-12-02 15:36:58 +0100

    rajout de la page admin dashboard avec Taleau de reservation, produits et commande

--- Views/reservationView.php
+++ Views/reservationView.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/v/bs4/dt-1.11.3/b-2.0.1/date-1.1.1/sl-1.3.3/datatables.min.css" />
+
+    <script type="text/javascript"
+        src="https://cdn.datatables.net/v/bs4/dt-1.11.3/b-2.0.1/date-1.1.1/sl-1.3.3/datatables.min.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
